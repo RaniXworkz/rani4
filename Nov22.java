659331ad7e21d326a466010a6ae7cdd42ede969c
RaniXorkz <raniattigeri.xworkz@gmail.com>
2021-12-20 23:24:50 +0530

    hello busy developers

---
+++ Nov22.java
@@ -0,0 +1,13 @@
+class Nov22
+{
+static String name="nissi";
+static String []names={"abhijna","raji","deepa","soumya"};
+public static void main(String[] args)
+{
+System.out.println(name);
+System.out.println(names[0]);
+System.out.println(names[1]);
+System.out.println(names[2]);
+System.out.println(names[3]);
+}
+}
